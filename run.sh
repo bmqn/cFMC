eb5efaaacdcc7397fe0091f223348648cc96f777
Max <18558188+maxbryarsmansell@users.noreply.github.com>
2022-12-16 14:31:57 +0000

    Rename built binary to cfmc

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ Main.cpp
 Lexer.cpp Parser.cpp Machine.cpp
 "
 echo 'Compiling...'
-c++ -std=c++17 -o build/fmc $SRC_FILES
+c++ -std=c++17 -o build/cfmc $SRC_FILES
 
 echo 'Running...'
-./build/fmc
+./build/cfmc
